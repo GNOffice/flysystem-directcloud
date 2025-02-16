18f204969412558c85c9f69c6aca40b831ab5dfa
USUI Tomoaki <t.usui@gn-office.pro>
2025-02-17 00:22:09 +0900

    テストの追加（ファイルだけ）

---
+++ tests/DirectCloudAdapterTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace GNOffice\FlysystemDirectCloud\Test;
+
+use League\Flysystem\AdapterTestUtilities\FilesystemAdapterTestCase;
+use League\Flysystem\FilesystemAdapter;
+
+class DirectCloudAdapterTest extends FilesystemAdapterTestCase
+{
+
+    protected static function createFilesystemAdapter(): FilesystemAdapter
+    {
+        // TODO: Implement createFilesystemAdapter() method.
+    }
+}
